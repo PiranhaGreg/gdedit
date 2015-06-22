30d694fdc5c8ae717bfdc39507747917f0e97c3c
Matěj Pokorný <pokorny.matej@gmail.com>
2015-06-22 23:35:13 +0200

    Added parsing mechanism with simple Drag & Drop example and file "levels.mrg" for testing.

---
+++ ts/levels.ts
@@ -0,0 +1,113 @@
+class Point {
+	constructor(public X: number, public Y: number) {}
+}
+
+class Level {
+	Start: Point;
+	End: Point;
+	Track = new Array<Point>();
+
+	constructor(public Name: string) {}
+
+	public AddPoint(point: Point) {
+		this.Track.push(point);
+	}
+
+	public static Decode(data: ArrayBuffer, offset: number): { level: Level, offset: number } {
+		var dataOffset = new DataView(data).getInt32(offset, false);
+		offset += 4;
+
+		var name = '';
+		var cstr = new Uint8Array(data, offset);
+		
+		for (var i = 0; cstr[i] !== 0; i++, offset++)
+			name += String.fromCharCode(cstr[i]);
+
+		offset++;
+		var level = new Level(name);
+
+		Level.decodeCoordinates(data, dataOffset, level);
+		return { level, offset };
+	}
+
+	static decodeCoordinates(data: ArrayBuffer, offset: number, level: Level) : void {
+		var view = new DataView(data, offset);
+		level.Start = new Point(view.getInt32(1, false), view.getInt32(5, false));
+		level.End = new Point(view.getInt32(9, false), view.getInt32(13, false));
+
+		var special = true;
+		var count = view.getInt16(17, false) - 1;
+		offset = 21;
+		
+		var offsetX = 0;
+		var offsetY = 0;
+
+		for (var i = 0; i < count; i++) {
+			var point: Point;
+
+			// reset offset with new value
+			if (special) {
+				point = new Point(view.getInt32(offset, false), view.getInt32(offset + 4, false));	
+				offset += 8;
+
+				offsetX = point.X;
+				offsetY = point.Y;
+
+				level.AddPoint(point);
+				special = false;
+				continue;
+			}
+
+			point = new Point(view.getInt8(offset), view.getInt8(offset + 1));
+			offset += 2;
+
+			// x is -1
+			if (point.X === -1) {
+				special = true;
+				offset--;
+				i--;
+				continue;
+			}
+
+			offsetX += point.X;
+			offsetY += point.Y;
+			level.AddPoint(point);
+		}
+	}
+}
+
+class Levels {
+	Easy = new Array<Level>();
+	Medium = new Array<Level>();
+	Hard = new Array<Level>();
+
+	public static Decode(data: ArrayBuffer) {
+		var result = new Levels();
+
+		var tmp = Levels.decodeLevels(data, 0);
+		result.Easy = tmp.levels;
+
+		tmp = Levels.decodeLevels(data, tmp.offset);
+		result.Medium = tmp.levels;
+
+		tmp = Levels.decodeLevels(data, tmp.offset);
+		result.Hard = tmp.levels;
+
+		return result;
+	}
+
+	static decodeLevels(data: ArrayBuffer, offset: number): { levels: Level[], offset: number } {
+		var count = new DataView(data).getInt32(offset, false);
+		var levels = new Array<Level>(count);
+
+		offset += 4;
+
+		for (var i = 0; i < count; i++) {
+			var tmp = Level.Decode(data, offset);
+			levels[i] = tmp.level;
+			offset = tmp.offset;
+		}
+
+		return { levels, offset };
+	}
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,45 +1,32 @@
-// create canvas element and append it to document body
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.style.position = 'fixed'; // (≖_≖ )
+/// <reference path="levels.ts" />
 
-// get canvas 2D context and set correct size
-var ctx = canvas.getContext('2d');
-resize();
+document.body;
+document.body.style.height = window.innerHeight + 'px';
+document.body.style.width = window.innerWidth + 'px';
 
-// last known position
-var pos = { x: 0, y: 0 };
+// Optional.   Show the copy icon when dragging over.  Seems to only work for chrome.
+document.body.addEventListener('dragover', function(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	document.body.style.background = 'red';
+	e.dataTransfer.dropEffect = 'copy';
+});
 
-window.addEventListener('resize', resize);
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseenter', setPosition);
+// Get file data on drop
+document.body.addEventListener('drop', function(e) {
+	e.stopPropagation();
+	e.preventDefault();
 
-// set new position from mouse event
-function setPosition(e: MouseEvent) {
-	pos.x = e.clientX;
-	pos.y = e.clientY;
-}
-
-// resize canvas
-function resize(e?: UIEvent) {
-	ctx.canvas.width = window.innerWidth;
-	ctx.canvas.height = window.innerHeight;
-}
-
-function draw(e: MouseEvent) {
-	// mouse left button must be pressed
-	if (e.buttons !== 1) return;
-
-	ctx.beginPath(); // begin
+	var files = e.dataTransfer.files; // Array of all files
 	
-	ctx.lineWidth = 5;
-	ctx.lineCap = 'round';
-	ctx.strokeStyle = '#c0392b';
+	for (var i = 0; i < files.length; i++) {
+		var reader = new FileReader();
 	
-	ctx.moveTo(pos.x, pos.y); // from
-	setPosition(e);
-	ctx.lineTo(pos.x, pos.y); // to
-	
-	ctx.stroke(); // draw it!
-}
+		reader.addEventListener('load', (e2) => {
+			var levels = Levels.Decode(reader.result);
+			console.log(levels);
+		});
+
+		reader.readAsArrayBuffer(files[i]); // start reading the file data.
+	}
+});
