f2dbb3b810437f20144bceb91693cec0ad40410f
Matěj Pokorný <pokorny.matej@gmail.com>
2015-06-24 01:56:17 +0200

    Large code refactoring.

--- ts/map.ts
+++ ts/map.ts
@@ -4,20 +4,84 @@ class CanvasLevel {
 	zoomFactors = [0.25, 0.33, 0.50, 0.67, 0.75, 0.90, 1, 1.10, 1.25, 1.50, 1.75, 2, 3, 4, 5];
 	zoomFactor = 6;
 
+	dim3 = true;
+
 	canvas: HTMLCanvasElement;
 	level: Level;
 	ctx: CanvasRenderingContext2D;
+
+	lastPoint = new Point(0, 0);
+	offset = new Point(0, 0);
 
 	constructor(canvas: HTMLCanvasElement, level: Level) {
 		this.canvas = canvas;
 		this.level = level;
 		this.ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
-		this.Resize(window.innerWidth, window.innerHeight);
 
-		// fix
-		window.addEventListener('resize', (e) => this.Resize(window.innerWidth, window.innerHeight));
+		this.Resize(window.innerWidth, window.innerHeight); // TODO
+		this.BestFit();
+
+		window.addEventListener('resize', (e) => this.Resize(window.innerWidth, window.innerHeight)); // TODO
 
 		canvas.addEventListener('wheel', (e: WheelEvent) => this.zoom(e));
+		
+		canvas.addEventListener('mousedown', (e) => {
+			this.lastPoint.X = e.x;
+			this.lastPoint.Y = e.y;
+		});
+
+		canvas.addEventListener('mousemove', (e) => {
+			if (e.buttons !== 1) return;
+
+			this.offset.X += e.x - this.lastPoint.X;
+			this.offset.Y += e.y - this.lastPoint.Y;
+
+			this.lastPoint.X = e.x;
+			this.lastPoint.Y = e.y;
+
+			this.redraw();
+		});
+
+		canvas.addEventListener('mouseenter', (e) => {
+
+		});
+	}
+
+	public BestFit() {
+		var track = this.level.Track;
+
+		var min = new Point(track[0].X, track[0].Y);
+		var max = new Point(track[0].X, track[0].Y);
+
+		for (var i = 1; i < track.length; i++) {
+			if (track[i].Y < min.Y) min.Y = track[i].Y;
+			if (track[i].X < min.X) min.X = track[i].X;
+			if (track[i].Y > max.Y) max.Y = track[i].Y;
+			if (track[i].X > max.X) max.X = track[i].X;
+		}
+
+		var height = max.Y - min.Y;
+		var width = max.X - min.X;
+
+		// best fit
+		this.zoomFactor = this.zoomFactors.length - 1;
+
+		while (this.zoomFactor > 0 && (
+			this.getZF() * height > this.canvas.height ||
+			this.getZF() * width > this.canvas.width))
+			this.zoomFactor--;
+
+		height *= this.getZF();
+		width *= this.getZF();
+
+		this.offset.X = (this.canvas.width - width) / 2;
+		this.offset.Y = (this.canvas.height - height) / 2;
+
+		// correct negative coordinates...
+		if (min.X < 0) this.offset.X -= min.X * this.getZF();
+		if (min.Y < 0) this.offset.Y -= min.Y * this.getZF();
+
+		this.redraw();
 	}
 
 	public Resize(width: number, height: number) {
@@ -27,66 +91,90 @@ class CanvasLevel {
 	}
 
 	zoom(e: WheelEvent) {
+		var tmp = this.zoomFactor;
+
 		if (e.deltaY < 0 && this.zoomFactor < this.zoomFactors.length - 1)
 			this.zoomFactor++;
 		else if (e.deltaY > 0 && this.zoomFactor > 0)
 			this.zoomFactor--;
 
-		console.log(Math.round(100 * this.zoomFactors[this.zoomFactor]) + '%');
-		this.redraw();
+		if (this.zoomFactor !== tmp) {
+			console.log(Math.round(100 * this.zoomFactors[this.zoomFactor]) + '%');
+			this.redraw();
+		}
 	}
 
 	getZF() : number {
 		return this.zoomFactors[this.zoomFactor];
 	}
 
+	drawGrid() {
+		// main horizontal line
+		this.drawLine(
+			new Point(0, this.offset.Y), 
+			new Point(this.canvas.width, this.offset.Y), 
+			'#ccc');
+
+		// main vertical line
+		this.drawLine(
+			new Point(this.offset.X, 0), 
+			new Point(this.offset.X, this.canvas.height), 
+			'#ccc');
+	}
+
 	redraw() {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
 		var track = this.level.Track;
-		var offset = -track[0].X;
 
 		var min = track[0].Y;
 		var max = track[0].Y;
 
 		for (var i = 1; i < track.length; i++) {
-			if (track[i].Y < min)
-				min = track[i].Y;
-
-			if (track[i].Y > max)
-				max = track[i].Y;
+			if (track[i].Y < min) min = track[i].Y;
+			if (track[i].Y > max) max = track[i].Y;
 		}
 
-		var rev = max - min;
+		var rev = (max - min) / 2;
 
-		// main horizontal line
-		this.drawLine(new Point(0, rev + min), new Point(this.ctx.canvas.width, rev + min), '#ccc');
+		var back = 20;
+		var front = 10;
 
-		// main vertical line
-		this.drawLine(new Point(offset, 0), new Point(offset, this.ctx.canvas.height), '#ccc');
+		if (!this.dim3)
+			front = 0;
+		
+		this.drawGrid();
 
+		// back line
+		if (this.dim3) {
+			for (var i = 0; i < track.length; i++) {
+				var from = this.recalc(new Point(track[i].X, rev - track[i].Y - back));
+				var to = this.recalc(new Point(track[i].X, rev - track[i].Y + front));
+				
+				this.drawLine(from, to, '#04aa04', this.getZF());
+			}
 
-		// track
-		for (var i = 0; i < track.length; i++) {
-			var from = new Point(this.getZF() * (track[i].X + offset), this.getZF() * (rev - track[i].Y + min));
-			var to = new Point(this.getZF() * (track[i].X + offset), this.getZF() * (rev - track[i].Y + min + 31));
-			
-			this.drawLine(from, to, '#04aa04', this.getZF());
+			for (var i = 0; i < track.length - 1; i++) {
+				var from = this.recalc(new Point(track[i].X, rev - track[i].Y - back));
+				var to = this.recalc(new Point(track[i + 1].X, rev - track[i + 1].Y - back));
+				
+				this.drawLine(from, to, '#04aa04', this.getZF());
+			}
 		}
 
+		// front line
 		for (var i = 0; i < track.length - 1; i++) {
-			// back line
-			var from = new Point(this.getZF() * (track[i].X + offset), this.getZF() * (rev - track[i].Y + min));
-			var to = new Point(this.getZF() * (track[i + 1].X + offset), this.getZF() * (rev - track[i + 1].Y + min));
-			
-			this.drawLine(from, to, '#04aa04', this.getZF());
-
-			// front line
-			var from = new Point(this.getZF() * (track[i].X + offset), this.getZF() * (rev - track[i].Y + min + 31));
-			var to = new Point(this.getZF() * (track[i + 1].X + offset), this.getZF() * (rev - track[i + 1].Y + min + 31));
+			var from = this.recalc(new Point(track[i].X, rev - track[i].Y + front));
+			var to = this.recalc(new Point(track[i + 1].X, rev - track[i + 1].Y + front));
 			
 			this.drawLine(from, to, '#00ff00', this.getZF());
 		}
+	}
+
+	recalc(point: Point) : Point {
+		return new Point(
+			this.offset.X + this.getZF() * point.X, 
+			this.offset.Y + this.getZF() * point.Y);
 	}
 
 	drawLine(from: Point, to: Point, color: string, lineWidth = 1) {
